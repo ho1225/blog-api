796db87adec2f198c13310373b1516c69fde1ab3
MSI\hosiu <hosiuchuen05@gmail.com>
2025-07-16 10:12:40 +0800

    create user and role entity and repository

--- src/main/java/com/schh/blogapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/schh/blogapi/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -71,5 +72,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             errors.put(field, message);
         });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ErrorDetails> handleAuthorizationDeniedException(
+            AuthorizationDeniedException exception,
+            WebRequest request
+    ) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
     }
 }
