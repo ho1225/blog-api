22f69aadd1a6424115ac1d4dfb84deec59fe8fcb
MSI\hosiu <hosiuchuen05@gmail.com>
2025-10-08 21:31:14 +0800

    create v2 thru URIpath

--- src/main/java/com/schh/blogapi/controller/PostController.java
+++ src/main/java/com/schh/blogapi/controller/PostController.java
@@ -2,6 +2,7 @@ package com.schh.blogapi.controller;
 
 
 import com.schh.blogapi.payload.PostDto;
+import com.schh.blogapi.payload.PostDtoV2;
 import com.schh.blogapi.payload.PostResponse;
 import com.schh.blogapi.service.PostService;
 import com.schh.blogapi.utils.AppConstant;
@@ -15,11 +16,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping()
 @Tag(
         name = "CRUD REST APIs for Post Resource"
 )
@@ -43,7 +45,7 @@ public class PostController {
             name = "Bear Authentication"
     )
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping
+    @PostMapping("/api/v1/posts")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
@@ -82,11 +84,26 @@ public class PostController {
     @SecurityRequirement(
             name = "Bear Authentication"
     )
-    @GetMapping("/{id}")
-    public ResponseEntity<PostDto> getPost(@PathVariable(name = "id") Long id) {
+    @GetMapping("/api/v1/posts/{id}")
+    public ResponseEntity<PostDto> getPostV1(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
+    @GetMapping("/api/v2/posts/{id}")
+    public ResponseEntity<PostDtoV2> getPostV2(@PathVariable(name = "id") Long id) {
+        PostDto postDto = postService.getPostById(id);
+        PostDtoV2 postDtoV2 = new PostDtoV2();
+        postDtoV2.setId(postDto.getId());
+        postDtoV2.setTitle(postDto.getTitle());
+        postDtoV2.setDescription(postDto.getDescription());
+        postDtoV2.setContent(postDto.getContent());
+        List<String> tags = new ArrayList<>();
+        tags.add("Java");
+        tags.add("Spring Boot");
+        tags.add("AWS");
+        postDtoV2.setTags(tags);
+        return ResponseEntity.ok(postDtoV2);
+    }
 
     @Operation(
             summary = "Update Post By ID REST API",
@@ -118,7 +135,7 @@ public class PostController {
             name = "Bear Authentication"
     )
     @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/v1/posts/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name = "id") Long id){
         postService.deletePostById(id);
         return new ResponseEntity<>("deleted successfully", HttpStatus.OK);
@@ -135,7 +152,7 @@ public class PostController {
     @SecurityRequirement(
             name = "Bear Authentication"
     )
-    @GetMapping("/category/{categoryId}")
+    @GetMapping("/api/v1/posts/category/{categoryId}")
     public ResponseEntity<List<PostDto>> getPostsByCategoryId(@PathVariable(name = "categoryId") Long categoryId) {
         return ResponseEntity.ok(postService.getPostsByCategoryId(categoryId));
     }
---
+++ src/main/java/com/schh/blogapi/payload/PostDtoV2.java
@@ -0,0 +1,52 @@
+package com.schh.blogapi.payload;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Set;
+
+
+@Data
+@Schema(
+        description = "PostDto Model Information"
+)
+public class PostDtoV2 {
+
+    private Long id;
+
+    @Schema(
+            description = "Blog Post Title"
+    )
+    @NotEmpty
+    @Size(min = 2, message = "Title should have at least 2 characters")
+    private String title;
+
+    @Schema(
+            description = "Blog Post Description"
+    )
+    @NotEmpty
+    @Size(min = 10, message = "Description should have at least 10 characters")
+    private String description;
+
+
+    @Schema(
+            description = "Blog Post Content"
+    )
+    @NotEmpty(message = "Content should not be null or empty")
+    private String content;
+
+    @Schema(
+            description = "Comments of Blog Post"
+    )
+    private Set<CommentDto> comments;
+
+    @Schema(
+            description = "Category ID of Blog Post"
+    )
+    private Long categoryId;
+
+    private List<String> tags;
+}
